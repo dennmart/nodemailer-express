bd040ffc40f44dd6cf50769ba033266040ffb59e
Dennis Martinez <dennis@dennmart.com>
2018-10-24 16:54:49 +0900

    Setting script properly from old Dokku server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var validator = require('validator');
 
 var app = express();
 app.use(bodyParser.json());
@@ -24,11 +25,20 @@ app.all('*', function(req, res, next) {
 });
 
 app.post('/contact', function(req, res) {
+  // Simple field validations.
+  if (validator.toString(req.body.name) == "") {
+    return res.status(422).send({ 'error': 'Please enter a valid name' }).end();
+  } else if (validator.toString(req.body.message) == "") {
+    return res.status(422).send({ 'error': 'Please enter a valid message' }).end();
+  } else if (!validator.isEmail(req.body.email)) {
+    return res.status(422).send({ 'error': 'Please enter a valid email address' }).end();
+  }
+
   var mailOptions = {
-    from: 'Portfolio Contact <contact@dennmart.com>',
-    to: 'dennis@dennmart.com',
-    subject: 'Contact from Portfolio',
-    text: 'From: ' + req.body.name + ' <' + req.body.email + '>\nmessage: ' + req.body.message
+    from: process.env.NODEMAILER_FROM,
+    to: process.env.NODEMAILER_TO,
+    subject: process.env.NODEMAILER_SUBJECT,
+    text: 'From: ' + req.body.name + ' <' + req.body.email + '>\nMessage: ' + req.body.message
   };
 
   transporter.sendMail(mailOptions, function(error, info) {
@@ -42,9 +52,10 @@ app.post('/contact', function(req, res) {
   });
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.NODEMAILER_SERVER_PORT, function() {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log("App running at http://%s:%s", host, port);
 });
+
